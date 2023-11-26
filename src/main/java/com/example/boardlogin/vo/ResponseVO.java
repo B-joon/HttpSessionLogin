49bfbc0b18641f01527bd35948945187d4d945f9
B-joon <dosel45@gmail.com>
2023-11-26 23:15:28 +0900

    Annotation 수정

--- src/main/java/com/example/boardlogin/vo/ResponseVO.java
+++ src/main/java/com/example/boardlogin/vo/ResponseVO.java
@@ -1,8 +1,11 @@
 package com.example.boardlogin.vo;
 
-import lombok.AllArgsConstructor;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
 public class ResponseVO<T> {
     HttpStatus status;

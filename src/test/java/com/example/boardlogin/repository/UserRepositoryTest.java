c14dd1018cb79ca1ea18d0187ab032cdef2dd0ba
B-joon <dosel45@gmail.com>
2023-11-26 23:14:38 +0900

    JPA Test Code 추가

---
+++ src/test/java/com/example/boardlogin/repository/UserRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.example.boardlogin.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void name() {
+
+        assertThat(userRepository).isNotNull();
+
+    }
+}
